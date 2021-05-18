351223fc5f535a68eb6b9228470579150aa18820
Seif <seif@>
2021-05-18 09:55:18 +0200

    classe client + adresse

--- src/main/java/sopra/auto_ecole/Application.java
+++ src/main/java/sopra/auto_ecole/Application.java
@@ -1,6 +1,42 @@
 package sopra.auto_ecole;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import sopra.auto_ecole.Application;
+import sopra.auto_ecole.repository.IClientRepository;
+import sopra.auto_ecole.repsitory.jpa.ClientRepositoryJpa;
+
+
+
 public class Application {
+	
+	private static Application instance = null;
+
+	private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("auto");
+
+	private final IClientRepository clientRepo = new ClientRepositoryJpa();
+	
+	
+	private Application() {
+
+	}
+
+	public static Application getInstance() {
+		if(instance == null) {
+			instance = new Application();
+		}
+
+		return instance;
+	}
+
+	public EntityManagerFactory getEmf() {
+		return emf;
+	}
+
+	public IClientRepository getClientRepo() {
+		return clientRepo;
+	}
 	
 	
 
---
+++ src/main/java/sopra/auto_ecole/repository/IClientRepository.java
@@ -0,0 +1,7 @@
+package sopra.auto_ecole.repository;
+
+import sopra.auto_ecole.model.Client;
+
+public interface IClientRepository extends IRepository<Client, Long>{
+
+}
